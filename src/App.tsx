ad6a580b3a3bc1dc73904a549f3c98649bd198f9
SwekcchaCornicle <singhswekccha@gmail.com>
2025-01-22 20:21:18 +0530

    route setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import { Routes, Route } from 'react-router-dom';
 import './globals.css';
+import SigninForm from './_auth/forms/SigninForm';
+import {Home} from './_root/pages';
+import SignupForm from './_auth/forms/SignupForm';
+import AuthLayout from './_auth/AuthLayout';
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello snapgram!
-    </h1>
+     <main className='flex h-screen'>
+       <Routes>
+        {/* Public routes*/ }
+        <Route element={<AuthLayout/>}>
+        <Route path = "/sign-in" element = {<SigninForm/>}/>
+        <Route path = "/sign-up" element = {<SignupForm/>}/>
+        </Route>
+
+         {/* Private routes*/ }
+        <Route element={<AuthLayout/>}>
+         <Route index element={<Home />}/>
+         </Route>
+       </Routes>
+     </main>
   )
 }
 
